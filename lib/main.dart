a1c1ed0786703efc2292295521aefc597015309d
hasnath.jami.chowdhury <hasnath.jami.chowdhury@gmail.com>
2024-08-31 17:45:56 +0600

    4. Exception Handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx_mvvm_masterclass/view/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'GetX Mvvm',
+      home: SplashScreen(),
     );
   }
 }
